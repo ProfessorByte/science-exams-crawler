cf7a9c7d6ead887d51fa3049fbed6d939ad3925c
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-05-01 01:46:09 -0400

    Add feature to save information in a JSON file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Buscando examenes...");
+import { getValidUrls } from "./pseudocrawler.js";
+import fs from "node:fs";
+
+const getCurrentTime = () => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  let month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  let day = String(currentDate.getDate()).padStart(2, "0");
+  let hour = String(currentDate.getHours()).padStart(2, "0");
+  let minutes = String(currentDate.getMinutes()).padStart(2, "0");
+  const currentTime = `Date: ${year}-${month}-${day} Time: ${hour}:${minutes}`;
+  return currentTime;
+};
+
+getValidUrls().then((validUrls) => {
+  fs.writeFileSync("validUrls.json", JSON.stringify(validUrls));
+  console.log(`Valid URLs saved (${getCurrentTime()})`);
+});
