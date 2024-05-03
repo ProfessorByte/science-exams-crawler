5be9435d0fc95c3d734eafa491f5b3f908967c46
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-05-02 23:52:24 -0400

    Refactor migrate.js to use setDoc instead of addDoc for saving validUrls

--- migrate.js
+++ migrate.js
@@ -1,7 +1,15 @@
-import { collection, addDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { db } from "./database.js";
 import validUrls from "./validUrls.json" assert { type: "json" };
 
 validUrls.forEach(async (urlData) => {
-  await addDoc(collection(db, "validUrls"), urlData);
+  const { year, semester, row, idResource } = urlData;
+  try {
+    await setDoc(
+      doc(db, "dataUrls", `${semester}-${year}-id${idResource}-row${row}`),
+      urlData
+    );
+  } catch (error) {
+    console.error("Error writing document: ", error);
+  }
 });
