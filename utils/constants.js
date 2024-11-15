4c3abe2cb56c0499d09fb9636d48e27f291a2190
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-11-15 14:05:35 -0400

    Refactor upperIdsLimit calculation to use validUrls data for dynamic value

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,11 @@
+import validUrls from "../validUrls.json" with { type: "json" };
+
 export const urlExamTemplate =
   "http://sagaa.fcyt.umss.edu.bo/adm_academica/archivos/examenes/{YEAR}-{SEMESTER}-{ID}/1/6-{FORM_VERSION}.pdf";
 export const urlSolutionTemplate =
   "http://sagaa.fcyt.umss.edu.bo/adm_academica/archivos/solucionario/{YEAR}-{SEMESTER}-{ID}/1/6-{FORM_VERSION}/0.pdf";
 
 export const lowerIdsLimit = 500;
-export const upperIdsLimit = 700;
+export const upperIdsLimit = validUrls.reduce((acc, urlData) => {
+  return urlData.idResource > acc ? urlData.idResource : acc;
+}, lowerIdsLimit + 45);
