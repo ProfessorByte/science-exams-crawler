aee5ca13e23894f26afec784769fd83c590b9a5a
Pablo Pardo <pablopardoalcocer@gmail.com>
2024-11-17 00:44:09 -0400

    Refactor valid URL handling to use async functions for loading and saving URLs from JSON

--- app/main.js
+++ app/main.js
@@ -2,28 +2,35 @@ import fs from "node:fs";
 import { getValidUrls } from "../utils/pseudocrawler.js";
 import { getCurrentTime } from "../utils/functions.js";
 
-try {
-  const validUrls = (
-    await import("../validUrls.json", { with: { type: "json" } })
-  ).default;
-
-  console.log(`Current valid URLs loaded (${getCurrentTime()})`);
-  getValidUrls().then((validUrlsCollected) => {
-    const onlyNewValidUrls = validUrlsCollected.filter(
-      (urlData) =>
-        !validUrls.some((urlDataSaved) => urlData.slug === urlDataSaved.slug)
-    );
-
-    const newValidUrls = [...validUrls, ...onlyNewValidUrls];
-
-    fs.writeFileSync("validUrls.json", JSON.stringify(newValidUrls, null, 2));
-    console.log(`Valid URLs saved (${getCurrentTime()})`);
-  });
-} catch (error) {
-  // console.error("Error loading JSON file:", error);
-  console.log(`Creating new JSON file... (${getCurrentTime()})`);
-  getValidUrls().then((validUrls) => {
-    fs.writeFileSync("validUrls.json", JSON.stringify(validUrls, null, 2));
-    console.log(`Valid URLs saved (${getCurrentTime()})`);
-  });
+async function loadValidUrls() {
+  try {
+    const validUrls = (
+      await import("../validUrls.json", { with: { type: "json" } })
+    ).default;
+    console.log(`Current valid URLs loaded (${getCurrentTime()})`);
+    return validUrls;
+  } catch (error) {
+    console.log(`Creating new JSON file... (${getCurrentTime()})`);
+    return [];
+  }
 }
+
+async function saveValidUrls(validUrls) {
+  fs.writeFileSync("validUrls.json", JSON.stringify(validUrls, null, 2));
+  console.log(`Valid URLs saved (${getCurrentTime()})`);
+}
+
+async function updateValidUrls() {
+  const validUrls = await loadValidUrls();
+  const validUrlsCollected = await getValidUrls();
+
+  const onlyNewValidUrls = validUrlsCollected.filter(
+    (urlData) =>
+      !validUrls.some((urlDataSaved) => urlData.slug === urlDataSaved.slug)
+  );
+
+  const newValidUrls = [...validUrls, ...onlyNewValidUrls];
+  await saveValidUrls(newValidUrls);
+}
+
+updateValidUrls();
